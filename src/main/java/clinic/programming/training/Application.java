3bf5a25a8207dce64a7b499b971769380fc4f1b9
Yurii <wervmer@gmail.com>
2025-04-01 18:06:55 +0300

    Changing compiler plugin to Java source 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,27 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public void greet() {
+		List<String> list = new ArrayList<>();
+		list.add("Hello");
+		
+		for (String e: list) {
+			System.out.println("Greeting " + e);
+		}
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
+		app.greet();
     }
 }
